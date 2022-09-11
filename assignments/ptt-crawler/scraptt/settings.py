345f2e455b55af6aa432c74041acb55fc435b42f
Retr0327 <philcoke35@gmail.com>
2022-09-12 02:25:25 +0800

    Config scrapy pipelines

--- assignments/ptt-crawler/scraptt/settings.py
+++ assignments/ptt-crawler/scraptt/settings.py
@@ -65,9 +65,9 @@ EXTENSIONS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'scraptt.pipelines.ScrapttPipeline': 300,
-# }
+ITEM_PIPELINES = {
+   'scraptt.pipelines.CsvPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
