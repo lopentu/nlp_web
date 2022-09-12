4d6917026f9eddd5cad44ccc1879fb38dfd97ec8
Retr0327 <philcoke35@gmail.com>
2022-09-12 17:38:34 +0800

    Update _exporter_for_item

--- assignments/ptt-crawler/scraptt/pipelines/csv.py
+++ assignments/ptt-crawler/scraptt/pipelines/csv.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from io import BufferedWriter
+from datetime import datetime
 from ..spiders import PttSpider
 from typing import Any, Dict, Tuple
 from scrapy.exporters import BaseItemExporter, CsvItemExporter
@@ -12,19 +14,28 @@ class CsvPipeline:
     def open_spider(self, spider: PttSpider) -> None:
         self.exporters_list: Dict[str, Tuple[BaseItemExporter, BufferedWriter]] = {}
 
-    def _exporter_for_item(self, item: Dict[str, Any]) -> CsvItemExporter:
+    def _exporter_for_item(
+        self, item: Dict[str, Any], spider: PttSpider
+    ) -> CsvItemExporter:
+        data_dir = spider.data_dir
         board = item.pop("board")
+        date = datetime.strptime(item["date"], "%Y-%m-%d %H:%M:%S")
+        year = date.year
+        month = date.month
+        dir_path = f"{data_dir}/{board}/{year}"
+        file_path = f"{dir_path}/{board}_{year}_{month}"
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
 
-        if board not in self.exporters_list:
-            file = open(f"{board}.csv", "wb")
+        if file_path not in self.exporters_list:
+            file = open(f"{file_path}.csv", "wb")
             exporter = CsvItemExporter(file)
             exporter.start_exporting()
-            self.exporters_list[board] = (exporter, file)
+            self.exporters_list[file_path] = (exporter, file)
 
-        return self.exporters_list[board][0]
+        return self.exporters_list[file_path][0]
 
     def process_item(self, item: Dict[str, Any], spider: PttSpider) -> Dict[str, Any]:
-        exporter = self._exporter_for_item(item)
+        exporter = self._exporter_for_item(item, spider)
         exporter.export_item(item)
         return item
 
