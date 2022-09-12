38b68bae063cde8d74ab04eee2a1cca71212a003
Retr0327 <philcoke35@gmail.com>
2022-09-12 10:26:37 +0800

    Add ptt urls

--- assignments/ptt-crawler/scraptt/configs/configs.py
+++ assignments/ptt-crawler/scraptt/configs/configs.py
@@ -1,1 +1,13 @@
+# --------------------------------------------------------------------
+# ptt urls
+
+
+PTT = "https://www.ptt.cc/bbs"
+PTT_BOARD = PTT + "/{}/index{}.html"  # template string (board, html index)
+
+
+# --------------------------------------------------------------------
+# cookies
+
+
 COOKIES = {"over18": "1"}
