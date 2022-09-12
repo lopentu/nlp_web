99b3d2af6015d64f9dc02ca1fb9ad2b8894abfb7
Retr0327 <philcoke35@gmail.com>
2022-09-12 10:19:46 +0800

    Update type hints

--- assignments/ptt-crawler/scraptt/middlewares/pyquery.py
+++ assignments/ptt-crawler/scraptt/middlewares/pyquery.py
@@ -1,4 +1,5 @@
 from pyquery import PyQuery
+from scrapy.http.response.html import HtmlResponse
 
 
 class PyqueryMiddleware:
@@ -6,7 +7,7 @@ class PyqueryMiddleware:
     The PyqueryMiddleware object injects PyQuery object into Scrapy `response`.
     """
 
-    def process_response(self, request, response, spider):
+    def process_response(self, request, response, spider) -> HtmlResponse:
         response.dom = PyQuery(response.text).make_links_absolute(
             "https://www.ptt.cc/bbs/"
         )
