f9c1e411f8f56788833f2b14ebce72b9d06f4ac6
Retr0327 <philcoke35@gmail.com>
2022-09-12 11:53:01 +0800

    Add fetch_ptt_boards

---
+++ assignments/ptt-crawler/scraptt/spiders/utils/requests/__init__.py
@@ -0,0 +1,4 @@
+from .requests import fetch_ptt_boards
+
+
+__all__ = ["fetch_ptt_boards"]
---
+++ assignments/ptt-crawler/scraptt/spiders/utils/requests/requests.py
@@ -0,0 +1,30 @@
+from scrapy import Request
+from ....configs import PTT_BOARD, COOKIES
+from typing import Callable, List, Optional
+
+
+def fetch_ptt_boards(
+    boards_list: List[str],
+    callback: Callable,
+    index_from: Optional[str] = None,
+    index_to: Optional[str] = None,
+):
+    """The fetch_ptt_boards function fetches the ptt boards htm indexes.
+
+    Args:
+        boards_list (list): a list of boards
+        callback (Callable): a scrapy parse function
+        index_from (str | None): the starting html index
+        index_to (str | None): the ending html index
+    Returns:
+        a scrapy Request.
+    """
+
+    for board in boards_list:
+        if index_from is not None and index_to is not None:
+            for index in range(int(index_from), int(index_to) + 1):
+                url = PTT_BOARD.format(board, index)
+                yield Request(url, cookies=COOKIES, callback=callback)
+        else:
+            url = PTT_BOARD.format(board, "")
+            yield Request(url, cookies=COOKIES, callback=callback)
