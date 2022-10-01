09893b65d5dd68ea3a88fc8c1daed460040db052
Retr0327 <philcoke35@gmail.com>
2022-10-01 18:31:25 +0800

    Update CsvPipeline

--- assignments/ptt-crawler/scraptt/pipelines/csv.py
+++ assignments/ptt-crawler/scraptt/pipelines/csv.py
@@ -1,18 +1,16 @@
 from pathlib import Path
-from io import BufferedWriter
+from .utils import get_items
+from typing import Any, Dict
 from datetime import datetime
+from .base import BasePipeline
 from ..spiders import PttSpider
-from typing import Any, Dict, Tuple
-from scrapy.exporters import BaseItemExporter, CsvItemExporter
+from scrapy.exporters import CsvItemExporter
 
 
-class CsvPipeline:
+class CsvPipeline(BasePipeline):
     """
     The CsvPipeline object writes the scraped item to csv.
     """
-
-    def open_spider(self, spider: PttSpider) -> None:
-        self.exporters_list: Dict[str, Tuple[BaseItemExporter, BufferedWriter]] = {}
 
     def _exporter_for_item(
         self, item: Dict[str, Any], spider: PttSpider
@@ -35,11 +33,22 @@ class CsvPipeline:
         return self.exporters_list[file_path][0]
 
     def process_item(self, item: Dict[str, Any], spider: PttSpider) -> Dict[str, Any]:
-        exporter = self._exporter_for_item(item, spider)
-        exporter.export_item(item)
-        return item
+        csv_item = get_items(
+            item,
+            [
+                "board",
+                "author",
+                "alias",
+                "title",
+                "date",
+                "ip",
+                "city",
+                "country",
+                "url",
+                "post_vote",
+            ],
+        )
 
-    def close_spider(self, spider: PttSpider) -> None:
-        for exporter, csv_file in self.exporters_list.values():
-            exporter.finish_exporting()
-            csv_file.close()
+        exporter = self._exporter_for_item(csv_item, spider)
+        exporter.export_item(csv_item)
+        return csv_item
