74cb2615394b0702eb98951e495f003c4ff1fd32
shenzizi <esperanzayaya22@gmail.com>
2022-11-18 16:54:46 +0800

    feat: add useFetch hook

---
+++ lab/pokemons/src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState , useEffect} from 'react';
+
+const useFetch = ({url, resolvedPath}) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setData(resolvedPath ? data[resolvedPath] : data);
+      })
+      .catch(error=> setError(error))
+      .finally(()=> setLoading(false))
+  }, [resolvedPath, url])
+
+  return {
+    loading, 
+    error,
+    data
+  }
+}
+
+export default useFetch;
