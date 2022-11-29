b4a3717346d4db384a52351b8f8ad984f449d65c
seantyh <seantyh@gmail.com>
2022-11-29 15:33:55 +0800

    add process_prompt

--- lab/deepdive/wander/src/chatbox.js
+++ lab/deepdive/wander/src/chatbox.js
@@ -12,36 +12,48 @@ export default function ChatBox() {
         reply: "Reply Message - 2"
       }
     ]);
-  
-  function onInputHandler(event){    
-    if (event.key==="Enter"){
-      console.log(event.target.value);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  function onInputHandler(event) {
+    if (event.key === "Enter") {
       let new_dialogue = Array.from(dialogue);
+      const prompt_text = event.target.value.trim()
       new_dialogue.push({
-        prompt: event.target.value.trim(),
+        prompt: prompt_text,
         reply: ""
       });
       event.target.value = "";
+      process_prompt(prompt_text, new_dialogue);
       setDialogue(new_dialogue);
-    }    
+    }
   }
 
+  function process_prompt(intext, dialogue_data) {
+    setIsProcessing(true);
+    setTimeout(() => {
+      let new_dialogue = Array.from(dialogue_data);
+      let last_turn = new_dialogue[new_dialogue.length - 1];
+      last_turn.reply = "hello there!"
+      setIsProcessing(false);
+      setDialogue(new_dialogue);
+    }, 100)
+  }
   return (
-    <div className="w-50 mx-auto mt-5 fs-4">
-      {dialogue.map((dialogue_x, idx) => {
-        return (
-          <ChatTurn key={`dialogue_${idx}`}
-            prompt={dialogue_x.prompt}
-            reply={dialogue_x.reply} />)
-      })}
-      <div>
-        <div className="w-100 mb-5 position-fixed bottom-0 start-0">
-          <div className="w-50 mx-auto mt-5 fs-4">
-            <input type="text" 
-              className="form-control form-control-lg" 
-              placeholder="Type here" 
-              onKeyDown={onInputHandler}/>
-          </div>
+    <div className="d-flex flex-column w-50 vh-100 mx-auto mt-1 fs-4 ">
+      <div className="flex-grow-1 overflow-auto">
+        {dialogue.map((dialogue_x, idx) => {
+          return (
+            <ChatTurn key={`dialogue_${idx}`}
+              prompt={dialogue_x.prompt}
+              reply={dialogue_x.reply} />)
+        })}
+      </div>
+      <div className="w-100 mb-5">
+        <div className="mx-auto mt-1 fs-4">
+          <input type="text" disabled={isProcessing}
+            className="form-control form-control-lg"
+            placeholder="Type here"
+            onKeyDown={onInputHandler} />
         </div>
       </div>
     </div >
