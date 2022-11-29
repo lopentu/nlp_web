c54b941b41099b73393f5255bc5d6fb0b61346f9
seantyh <seantyh@gmail.com>
2022-11-29 14:38:20 +0800

    extract dialogue state in ChatBox

--- lab/deepdive/wander/src/chatbox.js
+++ lab/deepdive/wander/src/chatbox.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import ChatTurn from "./chat-turn"
 
 export default function ChatBox() {
+  const [dialogue, setDialogue] = useState(
+    [
+      {
+        prompt: "Prompt Message - 1",
+        reply: "Reply Message - 1"
+      }, {
+        prompt: "Prompt Message - 2",
+        reply: "Reply Message - 2"
+      }
+    ]);
+
   return (
     <div className="w-50 mx-auto mt-5 fs-4">
-      <ChatTurn prompt="Prompt Message - 1" reply="Reply Message - 1"/>
-      <ChatTurn prompt="Prompt Message - 2" reply="Reply Message - 2"/>
-
+      {dialogue.map((dialogue_x, idx) => {
+        return (
+          <ChatTurn key={`dialogue_${idx}`}
+            prompt={dialogue_x.prompt}
+            reply={dialogue_x.reply} />)
+      })}
       <div>
         <div className="w-100 mb-5 position-fixed bottom-0 start-0">
           <div className="w-50 mx-auto mt-5 fs-4">
-            <input type="text" class="form-control form-control-lg" placeholder="Type here"/>
+            <input type="text" class="form-control form-control-lg" placeholder="Type here" />
           </div>
         </div>
       </div>
     </div >
   )
-}
+};
